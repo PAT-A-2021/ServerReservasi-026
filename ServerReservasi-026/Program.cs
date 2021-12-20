64f173e5cfb10fcadf990b51f07364cff2c1f534
Muhammad Risqullah <m.risqullah.ft19@mail.umy.ac.id>
2021-12-20 16:48:11 +0700

    Add project files.

---
+++ ServerReservasi-026/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using ServiceReservasi;
+using System.ServiceModel.Description;
+using System.ServiceModel.Channels;
+
+namespace ServerReservasi_026
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObj = null;
+            try
+            {
+                hostObj = new ServiceHost(typeof(Service1));
+                hostObj.Open();
+                Console.WriteLine("Server is ready!!!!");
+                Console.ReadLine();
+                hostObj.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObj = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
